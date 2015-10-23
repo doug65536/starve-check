cd8459df7deeccab8d08dfd0bdd24abfacab1e06
Doug Gale <doug16k@gmail.com>
2015-10-23 08:05:45 -0400

    Cross

--- toolchains/mingw32-32.cmake
+++ toolchains/mingw32-32.cmake
@@ -3,5 +3,5 @@ set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_AR "i686-w64-mingw32-ar" CACHE STRING "Use gcc AR")
 set(CMAKE_RANLIB "i686-w64-mingw32-ranlib" CACHE STRING "Use gcc RANLIB")
 
-set(CMAKE_CXX_COMPILER i686-w64-mingw32-g++-posix)
-set(CMAKE_C_COMPILER i686-w64-mingw32-gcc-posix)
+set(CMAKE_CXX_COMPILER i686-w64-mingw32-g++)
+set(CMAKE_C_COMPILER i686-w64-mingw32-gcc)
--- toolchains/mingw32-64.cmake
+++ toolchains/mingw32-64.cmake
@@ -3,5 +3,5 @@ set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_AR "x86_64-w64-mingw32-ar" CACHE STRING "Use cross AR")
 set(CMAKE_RANLIB "x86_64-w64-mingw32-ranlib" CACHE STRING "Use cross RANLIB")
 
-set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++-posix)
-set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc-posix)
+set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
