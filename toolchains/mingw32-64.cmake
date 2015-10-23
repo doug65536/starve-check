2f6e2fd8bcf1460bf4bfeec8cede07aefccea393
Doug Gale <doug16k@gmail.com>
2015-10-23 07:43:45 -0400

    Try to get cross compile working in travis

--- toolchains/mingw32-64.cmake
+++ toolchains/mingw32-64.cmake
@@ -1,7 +1,7 @@
-set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_NAME Windows)
 
-set(CMAKE_AR "gcc-ar" CACHE STRING "Use gcc AR")
-set(CMAKE_RANLIB "gcc-ranlib" CACHE STRING "Use gcc RANLIB")
+set(CMAKE_AR "x86_64-w64-mingw32-ar" CACHE STRING "Use cross AR")
+set(CMAKE_RANLIB "x86_64-w64-mingw32-ranlib" CACHE STRING "Use cross RANLIB")
 
 set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++-posix)
 set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc-posix)
