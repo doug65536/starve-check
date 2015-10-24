9bd9bf29993a8395a0726e6331ca656ab0f9fbc7
Doug Gale <doug16k@gmail.com>
2015-10-24 05:18:56 -0400

    Clang options

--- toolchains/clang-64.cmake
+++ toolchains/clang-64.cmake
@@ -3,5 +3,8 @@ set(CMAKE_SYSTEM_NAME Linux)
 #set(CMAKE_AR "gcc-ar" CACHE STRING "Use gcc AR")
 #set(CMAKE_RANLIB "gcc-ranlib" CACHE STRING "Use gcc RANLIB")
 
-set(CMAKE_CXX_COMPILER clang++)
-set(CMAKE_C_COMPILER clang)
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --stdlib=libc++ -pthread")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -pthread")
+
+set(CMAKE_CXX_COMPILER clang++-3.6)
+set(CMAKE_C_COMPILER clang-3.6)
