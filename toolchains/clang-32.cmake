04fcfa0acbf0793591fc05124c3284bfc13fd63b
Doug Gale <doug16k@gmail.com>
2015-10-24 02:08:37 -0400

    Fix clang buildall

--- toolchains/clang-32.cmake
+++ toolchains/clang-32.cmake
@@ -2,7 +2,8 @@ set(CMAKE_SYSTEM_NAME Linux)
 
 #set(CMAKE_AR "gcc-ar" CACHE STRING "Use gcc AR")
 #set(CMAKE_RANLIB "gcc-ranlib" CACHE STRING "Use gcc RANLIB")
-set(CMAKE_CXX_FLAGS "-m32" CACHE STRING "Use gcc RANLIB")
+set(CMAKE_CXX_FLAGS "-m32 -stdlib=libc++" CACHE STRING "C++ flags")
+set(CMAKE_C_FLAGS "-m32" CACHE STRING "C flags")
 
 set(CMAKE_CXX_COMPILER clang++)
 set(CMAKE_C_COMPILER clang)
