2c6dd2da112f2de207dc413846b33c0ac3d13045
Doug Gale <doug16k@gmail.com>
2015-10-24 10:54:13 -0400

    Detect lots of versions of compilers

--- toolchains/clang-32.cmake
+++ toolchains/clang-32.cmake
@@ -2,7 +2,7 @@ set(CMAKE_SYSTEM_NAME Linux)
 
 #set(CMAKE_AR "gcc-ar" CACHE STRING "Use gcc AR")
 #set(CMAKE_RANLIB "gcc-ranlib" CACHE STRING "Use gcc RANLIB")
-set(CMAKE_CXX_FLAGS "-m32 --stdlib=libc++" CACHE STRING "C++ flags")
+set(CMAKE_CXX_FLAGS "-m32 -stdlib=libc++" CACHE STRING "C++ flags")
 set(CMAKE_C_FLAGS "-m32" CACHE STRING "C flags")
 
 set(CMAKE_CXX_COMPILER clang++)
--- toolchains/clang-64.cmake
+++ toolchains/clang-64.cmake
@@ -3,9 +3,59 @@ set(CMAKE_SYSTEM_NAME Linux)
 #set(CMAKE_AR "gcc-ar" CACHE STRING "Use gcc AR")
 #set(CMAKE_RANLIB "gcc-ranlib" CACHE STRING "Use gcc RANLIB")
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --stdlib=libc++ -fPIE -pthread" CACHE STRING "Toolchain CXX flags")
-set(CMAKE_CXX_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -pie -pthread" CACHE STRING "Toolchain CXX flags")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -pthread" CACHE STRING "Toolchain C Flags")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --stdlib=libc++ -pthread")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -pthread")
 
-set(CMAKE_CXX_COMPILER clang++)
-set(CMAKE_C_COMPILER clang)
+find_program(CMAKE_CXX_COMPILER NAMES
+	clang++-4.9
+	clang++-4.8
+	clang++-4.7
+	clang++-4.6
+	clang++-4.5
+	clang++-4.4
+	clang++-4.3
+	clang++-4.2
+	clang++-4.1
+	clang++-4.0
+	clang++-3.9
+	clang++-3.8
+	clang++-3.7
+	clang++-3.6
+	clang++-3.5
+	clang++-3.4
+	clang++-3.3
+	clang++-3.2
+	clang++-3.1
+	clang++-3.0
+	clang++-2.9
+	clang++-2.8
+	clang++-2.7
+	clang++-2.6
+	clang++)
+
+find_program(CMAKE_CXX_COMPILER NAMES
+        clang-4.9
+        clang-4.8
+        clang-4.7
+        clang-4.6
+        clang-4.5
+        clang-4.4
+        clang-4.3
+        clang-4.2
+        clang-4.1
+        clang-4.0
+        clang-3.9
+        clang-3.8
+        clang-3.7
+        clang-3.6 
+        clang-3.5
+        clang-3.4
+        clang-3.3
+        clang-3.2
+        clang-3.1
+        clang-3.0
+        clang-2.9
+        clang-2.8
+        clang-2.7 
+	clang)
+
