473d240b7313ed9017d3596b942a1a779e9bf031
Doug Gale <doug16k@gmail.com>
2015-10-24 15:18:17 -0400

    All except clang working well

--- toolchains/mingw32-32.cmake
+++ toolchains/mingw32-32.cmake
@@ -1,13 +1,35 @@
 set(CMAKE_SYSTEM_NAME Windows)
 
-set(CMAKE_AR "i686-w64-mingw32-ar" CACHE STRING "Use gcc AR")
-set(CMAKE_RANLIB "i686-w64-mingw32-ranlib" CACHE STRING "Use gcc RANLIB")
+find_program(CMAKE_AR
+	i686-w64-mingw32-ar-posix
+	i686-w64-mingw32-ar-win32
+	i686-w64-mingw32-ar
+)
 
-set(CMAKE_CXX_COMPILER i686-w64-mingw32-g++)
-set(CMAKE_C_COMPILER i686-w64-mingw32-gcc)
-set(CMAKE_RC_COMPILER i686-w64-mingw32-windres)
+find_program(CMAKE_RANLIB
+	i686-w64-mingw32-ranlib
+)
+
+find_program(CMAKE_CXX_COMPILER 
+        i686-w64-mingw32-g++-posix 
+        i686-w64-mingw32-g++-win32 
+        i686-w64-mingw32-g++
+)
+
+find_program(CMAKE_C_COMPILER 
+        i686-w64-mingw32-gcc-posix    
+        i686-w64-mingw32-g++-win32 
+        i686-w64-mingw32-g++
+)
+
+find_program(CMAKE_RC_COMPILER 
+        i686-w64-mingw32-windres-posix
+        i686-w64-mingw32-windres-win32
+        i686-w64-mingw32-windres
+)
 
 set(CMAKE_CXX_FLAGS "-m32" CACHE STRING "Use libc++")
+set(CMAKE_C_FLAGS "-m32" CACHE STRING "Use libc++")
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
