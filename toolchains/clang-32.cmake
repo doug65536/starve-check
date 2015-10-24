1a245928eccaefff182ef62126f2ca709791fb5d
Doug Gale <doug16k@gmail.com>
2015-10-24 09:46:22 -0400

    Try build script in travis

--- toolchains/clang-32.cmake
+++ toolchains/clang-32.cmake
@@ -5,5 +5,5 @@ set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_CXX_FLAGS "-m32 --stdlib=libc++" CACHE STRING "C++ flags")
 set(CMAKE_C_FLAGS "-m32" CACHE STRING "C flags")
 
-set(CMAKE_CXX_COMPILER clang++-3.6)
-set(CMAKE_C_COMPILER clang-3.6)
+set(CMAKE_CXX_COMPILER clang++-4.6)
+set(CMAKE_C_COMPILER clang-4.6)
