fa1d18bc8037b543fd796099f846c3fa2ee2d124
Doug Gale <doug16k@gmail.com>
2015-10-24 09:25:34 -0400

    Cross-platform build lots of fixes

--- toolchains/clang-64.cmake
+++ toolchains/clang-64.cmake
@@ -3,8 +3,9 @@ set(CMAKE_SYSTEM_NAME Linux)
 #set(CMAKE_AR "gcc-ar" CACHE STRING "Use gcc AR")
 #set(CMAKE_RANLIB "gcc-ranlib" CACHE STRING "Use gcc RANLIB")
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --stdlib=libc++ -pthread")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -pthread")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --stdlib=libc++ -fPIE -pthread" CACHE STRING "Toolchain CXX flags")
+set(CMAKE_CXX_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -pie -pthread" CACHE STRING "Toolchain CXX flags")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -pthread" CACHE STRING "Toolchain C Flags")
 
-set(CMAKE_CXX_COMPILER clang++-3.6)
-set(CMAKE_C_COMPILER clang-3.6)
+set(CMAKE_CXX_COMPILER clang++-3.5)
+set(CMAKE_C_COMPILER clang-3.5)
